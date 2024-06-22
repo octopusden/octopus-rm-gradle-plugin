e3c7bac51a2a3b45f5ee8243d0607497092fac46
fishinitself <mikhail.ershov@gmail.com>
2024-06-22 14:05:09 +0300

    clean-up

--- ft/src/test/kotlin/org/octopusden/release/management/plugins/gradle/ReleaseManagementPluginTest.kt
+++ ft/src/test/kotlin/org/octopusden/release/management/plugins/gradle/ReleaseManagementPluginTest.kt
@@ -30,8 +30,8 @@ class ReleaseManagementPluginTest {
                 Arguments.of("multi-module", listOf("module-1", "module-2")),
                 Arguments.of("single-module-gradle-6.8.3", listOf("single-module-gradle-6.8.3")),
                 Arguments.of("multi-module-4.10.3", listOf("module-3", "module-4")),
-                Arguments.of("multi-module-with-root-publish-4.10.3", listOf("multi-module-with-root-publish-4.10.3", "module-5"))
-             //   Arguments.of("legacy-staging-plugin", listOf("deployer-dsl-core", "deployer-dsl-file"))
+                Arguments.of("multi-module-with-root-publish-4.10.3", listOf("multi-module-with-root-publish-4.10.3", "module-5")),
+                Arguments.of("legacy-staging-plugin", listOf("deployer-dsl-core", "deployer-dsl-file"))
         )
 
         @JvmStatic
