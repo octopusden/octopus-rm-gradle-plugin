e7b840812bbe19f0ec05d67922d70c4f1c29118a
fishinitself <mikhail.ershov@gmail.com>
2024-12-12 18:07:33 +0200

    clean test code

---
+++ ft/src/test/resources/publish/multi-module-regression/rmtest-core/src/main/java/org/octopusden/rmtest/core/utils/IntProperty.java
@@ -0,0 +1,16 @@
+package org.octopusden.rmtest.core.utils;
+
+public class IntProperty {
+
+    private String name;
+    private int value;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
---
+++ ft/src/test/resources/publish/multi-module-regression/rmtest-core/src/main/java/org/octopusden/rmtest/core/utils/StringProperty.java
@@ -0,0 +1,14 @@
+package org.octopusden.rmtest.core.utils;
+
+public class StringPair {
+    private String name;
+    private String value;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
