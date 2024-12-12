58e041232459dc1e45c8308394015ff906d10819
fishinitself <mikhail.ershov@gmail.com>
2024-12-12 20:08:20 +0200

    clean test code

--- ft/src/test/resources/publish/multi-module-regression/rmtest-log/src/main/java/org/octopusden/rmtest/logback/ICloudLogEvent.java
+++ ft/src/test/resources/publish/multi-module-regression/rmtest-log/src/main/java/org/octopusden/rmtest/logback/ICloudLogEvent.java
@@ -1,12 +1,6 @@
 package org.octopusden.rmtest.logback;
 
 public interface ICloudLogEvent {
-
-    String getTaskId();
-    String getSagaId();
-    String getSource();
-    long   getTimeout();
-    String getApi();
-
-    default String getArticle() { return null; }
+    String getEventName();
+    String getEventID();
 }
